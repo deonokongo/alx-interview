0bc089a7a9a23787230ab624b82f2163ff738aca
deonokongo <lilian.deon29@gmail.com>
2024-08-30 00:58:17 -0700

    correction

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,4 +1,30 @@
- print("Usage: nqueens N")
+#!/usr/bin/python3
+"""N queens solution finder module.
+"""
+import sys
+
+
+solutions = []
+"""The list of possible solutions to the N queens problem.
+"""
+n = 0
+"""The size of the chessboard.
+"""
+pos = None
+"""The list of possible positions on the chessboard.
+"""
+
+
+def get_input():
+    """Retrieves and validates this program's argument.
+
+    Returns:
+        int: The size of the chessboard.
+    """
+    global n
+    n = 0
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
